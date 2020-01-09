d33417f702ad2a4efffa4427326e9e4955d25746
Nikita <nikjk495@gmail.com>
2020-01-09 18:13:55 +0300

    * add component model to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-console.log("Hi")
+import React from "react";
+import ReactDOM from 'react-dom';
+
+const ToDoList = () => {
+    return (
+        <ul>
+            <li>Study react</li>
+            <li>Build ToDoList</li>
+        </ul>
+    );
+};
+
+const AppHeader = () => {
+    return <h1>My toDo List</h1>;
+};
+
+const SearchPanel = () => {
+    return <input placeholder='search'/>;
+};
+
+const  App = () => {
+    return <div>
+        <AppHeader/>
+        <SearchPanel/>
+        <ToDoList/>
+    </div>
+};
+
+ReactDOM.render(<App/>,document.getElementById('root'));
+
